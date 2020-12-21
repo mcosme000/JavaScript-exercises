60b2fa3efc2e2a8ac549b085f5a3b2b4899bb931
Maria Cosmen <mcosme00@gmail.com>
2020-12-21 22:32:08 +0900

    almost finished

--- src/27-ejercicio-arrays.js
+++ src/27-ejercicio-arrays.js
@@ -11,26 +11,66 @@ MAKE A PROGRAM THAT SHOWS THE FOLLOWING:
 6. Search for an specific element inside the array. 
 The program needs to tell us if that element exists or not. 
 If true, show us its index. 
-
 */
-//1. We add the 6 numbers to the array
+
+//
+
+// To show the array elements in the HTML body I will be using the code below,
+// so I created a function so that I can repeat it as many times as I need.
+
+function showArray(element, text = "") {
+  document.write("<h3> Array content " + text + "</h3>");
+  element.forEach((element, index) => {
+    document.write(index + " - " + element + "<br>");
+  });
+}
+
+//① Add the 6 numbers to the array
 var numbers = [];
 
 do {
-  var newNumber = parseInt(prompt("Give me a number"));
+  var newNumber = parseInt(prompt("Give me any number!"));
   numbers.push(newNumber);
 } while (numbers.length <= 5);
 
+//
+
+//② Show the numbers of the array on the console
 console.log(numbers);
 
-// 3. We show the array in order
-numbers.forEach((numero, index) => {
-  document.write(numero + "<br>");
+//
+
+//③ Show the numbers of the array on the HTML body
+showArray(numbers);
+
+//
+
+// ④ Sort the elements
+numbers.sort(function (a, b) {
+  return a - b;
 });
+showArray(numbers, "sorted");
+// I also add the "extra text"
+/*
+With this function we can sort the numbers 
+function(a,b) {return a-b}; 
+if we don't write it, the numbers only will be sorted alfabetically. 
+*/
 
-// ordenar los elementos del array:
-console.log(numbers.sort());
+//
 
-// invertir el orden de los elementos:
+// ⑤ Reverse the order of the elements
+numbers.reverse();
+showArray(numbers, "reversed");
 
-console.log(numbers.reverse());
+//
+
+// ⑥ Show how many elements there are in the array
+document.write("<h3>This array has " + numbers.length + " elements");
+
+//
+
+// ⑦ Look for a specific element inside the array
+var look = parseInt(prompt("Look for a number"));
+
+// -* -* -* -* -* -* -* -* -* -* -* -* -* -* -* -* -* -* -*
